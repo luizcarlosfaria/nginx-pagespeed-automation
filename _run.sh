c69ab954d657a74f8d946251eaf0526f81c3bb41
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2016-09-21 22:07:44 -0300

    package.json review

--- _run.sh
+++ _run.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-npm install && node ./index.js
+echo '' && echo '' && echo '' && echo ''
+echo '  ===================================================================='
+echo '  |                                                                  |'
+echo '  |         nginx-pagespeed Automation Automation Tool               |'
+echo '  |                                                                  |'
+echo '  |   https://github.com/docker-gallery/nginx-pagespeed              |'
+echo '  |   https://github.com/docker-gallery/nginx-pagespeed-automation   |'
+echo '  ===================================================================='
+echo ''
+
+if [ \( -f "./node_modules/ejs/ejs.js" \) -a  \( -f "./node_modules/is_js/is.js" \) -a  \( -f "./node_modules/linq/linq.js" \) ]
+then
+	echo "All packages found. Starting template engine..."
+else
+	echo "One or more required packages not found, trying to restore packages"
+    npm install     
+    echo "Starting template engine..."
+fi
+node ./index.js
